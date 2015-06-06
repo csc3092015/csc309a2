99ea02e91120a86086b9f7aa076b2f05fda88551
elio303 <eliokazu@gmail.com>
2015-06-06 12:50:15 -0400

    Update a2.js

--- a2.js
+++ a2.js
@@ -1,3 +1,15 @@
 function startGame() {
         /* make a sound to start the game and maybe some other things? */
 }
+
+function pauseUnpause(){
+        /* If game is paused, resume. Otherwise pause. */
+}
+
+function endGame(){
+        /* When the timer hits 0, this should be called to end the game */
+}
+
+function animate(){
+        /* Use this to change the frame of the game per how-ever-many milliseconds to animate game */
+}
