2936076cd5223c64917be5ad3edf997979284dca
Elio Tanke <eliokazu@gmail.com>
2015-06-07 21:15:07 -0400

    Changed a2.css so that it contains functions to draw bugs and food on canvas

--- a2.js
+++ a2.js
@@ -5,6 +5,9 @@ window.onload = function() {
 	var startButton = document.getElementById("startButton");
 	var pauseButton = document.getElementById("pauseButton");
 	var backButton = document.getElementById("backButton");
+	
+	/* Bug and Food Canvases */
+	var gameCanvas = document.getElementById("gameCanvas").getContext("2d");
 
 	function startBackButtonOnclick() {
 		if (currentPage === 'startPage') {
@@ -32,6 +35,26 @@ window.onload = function() {
 
 	function animate(){
 	        /* Use this to change the frame of the game per how-ever-many milliseconds to animate game */
+	}
+	
+	function drawApple(){
+		
+	}
+	
+	function drawOrange(){
+		
+	}
+	
+	function drawBanana(){
+		
+	}
+	
+	function drawBug1(){
+		
+	}
+	
+	function drawBug2(){
+		
 	}
 
 	startButton.onclick = startBackButtonOnclick;
